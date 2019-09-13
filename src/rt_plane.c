67b3c598d9ca7d186705a64bee132600522d7291
Denys MOLYBOHA <dmolyboh@e1r6p15.unit.ua>
2019-09-13 20:49:35 +0300

    disk plane

--- src/rt_plane.c
+++ src/rt_plane.c
@@ -6,7 +6,7 @@
 /*   By: dmolyboh <dmolyboh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/25 17:52:26 by mhonchar          #+#    #+#             */
-/*   Updated: 2019/09/13 20:04:17 by dmolyboh         ###   ########.fr       */
+/*   Updated: 2019/09/13 20:49:03 by dmolyboh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,20 @@ void	rt_intersect_ray_plane(t_ray ray, t_objects *plane,
 		t = -dot(oc, plane->orient) / denominator;
 		if (t > dist_range[0] && t < dist_range[1] && t < inter->dist)
 		{
-			inter->dist = t;
-			inter->closest_obj = plane;
+
+
+		//float t;
+		t_vec hitpoint;
+
+		t = -dot(oc, plane->orient) / dot(ray.direction,  plane->orient);
+		hitpoint = t * ray.direction + oc;
+		if (vec_length(hitpoint) < 5)
+		{
+				inter->dist = t;
+				inter->closest_obj = plane;	
 		}
+	}
+
+			
 	}
 }
